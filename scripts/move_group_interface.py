baf1c0cca45b70e25bc64981e58bd552a5d75a2c
Joseph <jeg4@rice.edu>
2021-01-02 21:22:35 -0600

    Code import and cleanup

---
+++ scripts/move_group_interface.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+def all_close(goal, actual, tolerance):
+  """
+  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+  @param: goal       A list of floats, a Pose or a PoseStamped
+  @param: actual     A list of floats, a Pose or a PoseStamped
+  @param: tolerance  A float
+  @returns: bool
+  """
+  all_equal = True
+  if type(goal) is list:
+    for index in range(len(goal)):
+      if abs(actual[index] - goal[index]) > tolerance:
+        return False
+
+  elif type(goal) is geometry_msgs.msg.PoseStamped:
+    return all_close(goal.pose, actual.pose, tolerance)
+
+  elif type(goal) is geometry_msgs.msg.Pose:
+    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+  return True
+
+
+class MoveGroupInterface():
+  """
+  Provides useful methods for robot interaction that utilize the MoveGroupCommander class
+  """
+
+  def __init__(self):
+    """
+    Initializes the interface to control a Franka Emika Panda robot
+    """
+    
+    # First initialize `moveit_commander`_ and a `rospy`_ node:
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+
+    # Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+    # kinematic model and the robot's current joint states
+    robot = moveit_commander.RobotCommander()
+
+    # Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+    # for getting, setting, and updating the robot's internal understanding of the
+    # surrounding world:
+    scene = moveit_commander.PlanningSceneInterface()
+
+    # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+    # to a planning group (group of joints). 
+    # This interface can be used to plan and execute motions:
+    group_name = "panda_arm"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    # Create a `DisplayTrajectory`_ ROS publisher which is used to display
+    # trajectories in Rviz:
+    display_trajectory_publisher = rospy.Publisher(
+      '/move_group/display_planned_path',
+      moveit_msgs.msg.DisplayTrajectory,
+      queue_size=20)
+
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_group.get_planning_frame()
+    print("Planning frame: " + planning_frame)
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+    print("End effector link: " + eef_link)
+
+    # We can get a list of all the groups in the robot:
+    group_names = robot.get_group_names()
+    print("Available Planning Groups:", robot.get_group_names())
+
+    # Sometimes for debugging it is useful to print the entire state of the
+    # robot:
+    print("Printing robot state")
+    print(robot.get_current_state())
+    print("")
+
+    # Misc variables
+    self.box_name = ''
+    self.robot = robot
+    self.scene = scene
+    self.move_group = move_group
+    self.display_trajectory_publisher = display_trajectory_publisher
+    self.planning_frame = planning_frame
+    self.eef_link = eef_link
+    self.group_names = group_names
+    
+
+  def go_to_pose(self, x, y, z):
+    """
+    Makes end effector go to the pose position specified by the parameters
+    @param: x   The goal x position
+    @param: y   The goal y position
+    @param: z   The goal z position
+    """
+    
+    # Set the pose goal
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.w = 1.0
+    pose_goal.position.x = x
+    pose_goal.position.y = y
+    pose_goal.position.z = z
+    self.move_group.set_pose_target(pose_goal)
+
+    # Now, we call the planner to compute the plan and execute it.
+    plan = self.move_group.go(wait=True)
+    
+    # Calling `stop()` ensures that there is no residual movement
+    self.move_group.stop()
+    
+    # It is always good to clear your targets after planning with poses.
+    # Note: there is no equivalent function for clear_joint_value_targets()
+    self.move_group.clear_pose_targets()
+
+    # We use the class variable rather than the copied state variable
+    current_pose = self.move_group.get_current_pose().pose
+    
+    # Return if the position we went to is within tolerances of the position we wanted
+    return all_close(pose_goal, self.move_group.get_current_pose().pose, 0.01)
+  
+
+  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
+    """
+    Waits until we reach the expected state. Needed because the node may die before
+    publishing a collision object update message.
+
+    Args:
+      box_is_known (bool, optional): Expected value for whether the box is in the scene. 
+        Defaults to False.
+      box_is_attached (bool, optional): Expected value for whether the box is attached 
+        to the robot. Defaults to False.
+      timeout (int, optional): Number of seconds to wait as a maximum for the expected
+        state to be reached. Defaults to 4.
+
+    Returns:
+      bool: Whether the expected state was reached before the timeout
+    """
+    
+    start = rospy.get_time()
+    seconds = rospy.get_time()
+    
+    
+    while (seconds - start < timeout) and not rospy.is_shutdown():
+      # Test if the box is in attached objects
+      attached_objects = self.scene.get_attached_objects([self.box_name])
+      is_attached = len(attached_objects.keys()) > 0
+
+      # Test if the box is in the scene.
+      # Note that attaching the box will remove it from known_objects
+      is_known = self.box_name in self.scene.get_known_object_names()
+
+      # Test if we are in the expected state
+      if (box_is_attached == is_attached) and (box_is_known == is_known):
+        return True
+
+      # Sleep so that we give other threads time on the processor
+      rospy.sleep(0.1)
+      seconds = rospy.get_time()
+
+    # If we exited the while loop without returning then we timed out
+    return False
+  
+
+  def add_box(self, timeout=4):
+    """
+    Adds a box at the location of the panda's left box stores it in an object variable.
+
+    Args:
+      timeout (int, optional): Number of seconds to wait as a maximum for the box to
+      be added. Defaults to 4.
+
+    Returns:
+      bool: Whether the box was successfully added
+    """
+    
+    # Creates the pose for the box to be placed at
+    box_pose = geometry_msgs.msg.PoseStamped()
+    box_pose.header.frame_id = "panda_leftfinger"
+    box_pose.pose.orientation.w = 1.0
+    box_pose.pose.position.z = 0.07 # slightly above the end effector
+    
+    # Adds the box
+    self.box_name = "box"
+    self.scene.add_box(self.box_name, box_pose, size=(0.1, 0.1, 0.1))
+
+    # Checks whether the box was successfully added
+    return self.wait_for_state_update(box_is_known=True, timeout=timeout)
+
+
+  def attach_box(self, timeout=4):
+    """
+    Attaches the box to the Panda wrist. Tells the planning scene to ignore collisions 
+    between the Panda and the box.
+
+    Args:
+      timeout (int, optional): Number of seconds to wait as a maximum for the box to
+      be attached. Defaults to 4.
+
+    Returns:
+      bool: Whether the box was successfully attached
+    """
+
+    # Attaches the box to the hand group of the robot
+    grasping_group = 'hand'
+    touch_links = self.robot.get_link_names(group=grasping_group)
+    self.scene.attach_box(self.eef_link, self.box_name, touch_links=touch_links)
+
+    # Checks whether the box was successfully added
+    return self.wait_for_state_update(box_is_attached=True, box_is_known=False, timeout=timeout)
+
+
+  def detach_box(self, timeout=4):
+    """
+    Detaches the box from the Panda wrist. Tells the planning scene not to ignore collisions
+    between the Panda and the box.
+
+    Args:
+        timeout (int, optional): Number of seconds to wait as a maximum for the box to
+        be detached. Defaults to 4.
+
+    Returns:
+        bool: Whether the box was successfully detached
+    """
+
+    # Detach the box
+    self.scene.remove_attached_object(self.eef_link, name=self.box_name)
+
+    # Checks whether the box was successfully detached
+    return self.wait_for_state_update(box_is_known=True, box_is_attached=False, timeout=timeout)
+
+
+  def remove_box(self, timeout=4):
+    """
+    Remove the box from the planning scene. The box must be detached to do this.
+
+    Args:
+        timeout (int, optional): Number of seconds to wait as a maximum for the box to
+        be removed. Defaults to 4.
+
+    Returns:
+        bool: Whether the box was successfully removed
+    """
+
+    # Remvoe the box from the world
+    self.scene.remove_world_object(self.box_name)
+
+    # We wait for the planning scene to update.
+    return self.wait_for_state_update(box_is_attached=False, box_is_known=False, timeout=timeout)
--- scripts/parse_poses.py
+++ scripts/parse_poses.py
@@ -1,196 +1,68 @@
 #!/usr/bin/env python
 
+import move_group_interface
 import sys
-import copy
 import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
-from math import pi
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
-
-
-def all_close(goal, actual, tolerance):
-    """
-    Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-    @param: goal       A list of floats, a Pose or a PoseStamped
-    @param: actual     A list of floats, a Pose or a PoseStamped
-    @param: tolerance  A float
-    @returns: bool
-    """
-    all_equal = True
-    if type(goal) is list:
-        for index in range(len(goal)):
-            if abs(actual[index] - goal[index]) > tolerance:
-                return False
-
-    elif type(goal) is geometry_msgs.msg.PoseStamped:
-        return all_close(goal.pose, actual.pose, tolerance)
-
-    elif type(goal) is geometry_msgs.msg.Pose:
-        return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
-
-    return True
-
 
 def parse_csv(file_path):
-    """
-    Parses a file containing comma seperated triples of floats representing pose goals
-    @param: file_path   A string representing the absolute path to the file to be parsed
-    @returns: A list of lists of floats
-    """
-    file = open(file_path, "r")
-    file_text = file.read()
-    file.close()
+  """
+  Parses a file containing comma seperated triples of floats representing pose goals
+  @param: file_path   A string representing the absolute path to the file to be parsed
+  @returns: A list of lists of floats
+  """
+  file = open(file_path, "r")
+  file_text = file.read()
+  file.close()
 
-    # print(file_text)
+  # print(file_text)
 
-    file_lines = file_text.split("\n")
-    result = []
-    for line in file_lines:
-        result_line = []
-        line_split = line.split(",")
-        for entry in line_split:
-            entry.strip()
-            result_line.append(float(entry))
-        result.append(result_line)
+  file_lines = file_text.split("\n")
+  result = []
+  for line in file_lines:
+    result_line = []
+    line_split = line.split(",")
+    for entry in line_split:
+      entry.strip()
+      result_line.append(float(entry))
+    result.append(result_line)
 
-    return result
-
-
-class MoveGroupPythonInterface():
-    """
-    Provides useful methods for robot interaction that utilize the MoveGroupCommander class
-    """
-
-    def __init__(self):
-        """
-        Initializes the interface to control a Franka Emika Panda robot
-        """
-        
-        # First initialize `moveit_commander`_ and a `rospy`_ node:
-        moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
-
-        # Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-        # kinematic model and the robot's current joint states
-        robot = moveit_commander.RobotCommander()
-
-        # Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-        # for getting, setting, and updating the robot's internal understanding of the
-        # surrounding world:
-        scene = moveit_commander.PlanningSceneInterface()
-
-        # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        # to a planning group (group of joints). 
-        # This interface can be used to plan and execute motions:
-        group_name = "panda_arm"
-        move_group = moveit_commander.MoveGroupCommander(group_name)
-
-        # Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        # trajectories in Rviz:
-        display_trajectory_publisher = rospy.Publisher(
-            '/move_group/display_planned_path',
-            moveit_msgs.msg.DisplayTrajectory,
-            queue_size=20)
-
-        # We can get the name of the reference frame for this robot:
-        planning_frame = move_group.get_planning_frame()
-        print("Planning frame: " + planning_frame)
-
-        # We can also print the name of the end-effector link for this group:
-        eef_link = move_group.get_end_effector_link()
-        print("End effector link: " + eef_link)
-
-        # We can get a list of all the groups in the robot:
-        group_names = robot.get_group_names()
-        print("Available Planning Groups:", robot.get_group_names())
-
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        print("Printing robot state")
-        print(robot.get_current_state())
-        print("")
-
-        # Misc variables
-        self.box_name = ''
-        self.robot = robot
-        self.scene = scene
-        self.move_group = move_group
-        self.display_trajectory_publisher = display_trajectory_publisher
-        self.planning_frame = planning_frame
-        self.eef_link = eef_link
-        self.group_names = group_names
-
-    def go_to_pose(self, x, y, z):
-        """
-        Makes end effector go to the pose position specified by the parameters
-        @param: x   The goal x position
-        @param: y   The goal y position
-        @param: z   The goal z position
-        """
-        
-        # Set the pose goal
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.orientation.w = 1.0
-        pose_goal.position.x = x
-        pose_goal.position.y = y
-        pose_goal.position.z = z
-        self.move_group.set_pose_target(pose_goal)
-
-        # Now, we call the planner to compute the plan and execute it.
-        plan = self.move_group.go(wait=True)
-        
-        # Calling `stop()` ensures that there is no residual movement
-        self.move_group.stop()
-        
-        # It is always good to clear your targets after planning with poses.
-        # Note: there is no equivalent function for clear_joint_value_targets()
-        self.move_group.clear_pose_targets()
-
-        # We use the class variable rather than the copied state variable
-        current_pose = self.move_group.get_current_pose().pose
-        
-        # Return if the position we went to is within tolerances of the position we wanted
-        return all_close(pose_goal, current_pose, 0.01)
+  return result
 
 
 def main(file_path):
-    """
-    Moves the end effector of the panda to the poses specified in the file given
+  """
+  Moves the end effector of the panda to the poses specified in the file given
 
-    Args:
-        file_path (string): the file path to the file containing poses
-    """
+  Args:
+    file_path (string): the file path to the file containing poses
+  """
+  
+  try:
+    # Initializes the move group interface
+    robot_interface = move_group_interface.MoveGroupInterface()
     
-    try:
-        
-        # Initializes the move group interface
-        robot_interface = MoveGroupPythonInterface()
-        
-        # Parses the poses from the input file path
-        pose_goals = parse_csv(file_path)
+    # Parses the poses from the input file path
+    pose_goals = parse_csv(file_path)
 
-        # Goes to every pose in the list of poses
-        for pose_goal in pose_goals:
-            print(pose_goal)
-            robot_interface.go_to_pose(pose_goal[0], pose_goal[1], pose_goal[2])
+    # Goes to every pose in the list of poses
+    for pose_goal in pose_goals:
+      print(pose_goal)
+      robot_interface.go_to_pose(pose_goal[0], pose_goal[1], pose_goal[2])
 
-    # Ends the program if we get one of these errors
-    except rospy.ROSInterruptException:
-        return
-    except KeyboardInterrupt:
-        return
+  # Ends the program if we get one of these errors
+  except rospy.ROSInterruptException:
+    return
+  except KeyboardInterrupt:
+    return
 
 
 if __name__ == '__main__':
-    
-    # Default file we are parsing poses from
-    file_path = "/home/joseph/ws_moveit/src/jgioia_panda_projects/csv/ex1.csv"
-    
-    # Overrides default if file is specified in arguments
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1]
-            
-    main(file_path)
+  
+  # Default file we are parsing poses from
+  file_path = "/home/joseph/ws_moveit/src/jgioia_panda_projects/csv/ex1.csv"
+  
+  # Overrides default if file is specified in arguments
+  if len(sys.argv) > 1:
+    file_path = sys.argv[1]
+      
+  main(file_path)
