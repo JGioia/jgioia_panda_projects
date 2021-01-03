f5611bdef1de46664661ab4ff1a9f5c278a809f3
Joseph <jeg4@rice.edu>
2021-01-02 20:20:28 -0600

    code cleanup

--- scripts/parse_poses.py
+++ scripts/parse_poses.py
@@ -59,63 +59,57 @@ def parse_csv(file_path):
     return result
 
 
-class MoveGroupPythonInterface(object):
-    """MoveGroupPythonInterfaceTutorial"""
+class MoveGroupPythonInterface():
+    """
+    Provides useful methods for robot interaction that utilize the MoveGroupCommander class
+    """
 
     def __init__(self):
-        super(MoveGroupPythonInterface, self).__init__()
-
-        ##
-        ## First initialize `moveit_commander`_ and a `rospy`_ node:
+        """
+        Initializes the interface to control a Franka Emika Panda robot
+        """
+        
+        # First initialize `moveit_commander`_ and a `rospy`_ node:
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
 
-        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-        ## kinematic model and the robot's current joint states
+        # Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        # kinematic model and the robot's current joint states
         robot = moveit_commander.RobotCommander()
 
-        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-        ## for getting, setting, and updating the robot's internal understanding of the
-        ## surrounding world:
+        # Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        # for getting, setting, and updating the robot's internal understanding of the
+        # surrounding world:
         scene = moveit_commander.PlanningSceneInterface()
 
-        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        ## to a planning group (group of joints).  In this tutorial the group is the primary
-        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
-        ## If you are using a different robot, change this value to the name of your robot
-        ## arm planning group.
-        ## This interface can be used to plan and execute motions:
+        # Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        # to a planning group (group of joints). 
+        # This interface can be used to plan and execute motions:
         group_name = "panda_arm"
         move_group = moveit_commander.MoveGroupCommander(group_name)
 
-        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        ## trajectories in Rviz:
+        # Create a `DisplayTrajectory`_ ROS publisher which is used to display
+        # trajectories in Rviz:
         display_trajectory_publisher = rospy.Publisher(
             '/move_group/display_planned_path',
             moveit_msgs.msg.DisplayTrajectory,
             queue_size=20)
 
-        ## END_SUB_TUTORIAL
-
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
-        ## Getting Basic Information
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
         # We can get the name of the reference frame for this robot:
         planning_frame = move_group.get_planning_frame()
-        print("============ Planning frame: " + planning_frame)
+        print("Planning frame: " + planning_frame)
 
         # We can also print the name of the end-effector link for this group:
         eef_link = move_group.get_end_effector_link()
-        print("============ End effector link: " + eef_link)
+        print("End effector link: " + eef_link)
 
         # We can get a list of all the groups in the robot:
         group_names = robot.get_group_names()
-        print("============ Available Planning Groups:", robot.get_group_names())
+        print("Available Planning Groups:", robot.get_group_names())
 
         # Sometimes for debugging it is useful to print the entire state of the
         # robot:
-        print("============ Printing robot state")
+        print("Printing robot state")
         print(robot.get_current_state())
         print("")
 
@@ -130,40 +124,60 @@ class MoveGroupPythonInterface(object):
         self.group_names = group_names
 
     def go_to_pose(self, x, y, z):
-        ## Planning to a Pose Goal
-        ## We can plan a motion for this group to a desired pose for the
-        ## end-effector:
+        """
+        Makes end effector go to the pose position specified by the parameters
+        @param: x   The goal x position
+        @param: y   The goal y position
+        @param: z   The goal z position
+        """
+        
+        # Set the pose goal
         pose_goal = geometry_msgs.msg.Pose()
         pose_goal.orientation.w = 1.0
         pose_goal.position.x = x
         pose_goal.position.y = y
         pose_goal.position.z = z
-
         self.move_group.set_pose_target(pose_goal)
 
-        ## Now, we call the planner to compute the plan and execute it.
+        # Now, we call the planner to compute the plan and execute it.
         plan = self.move_group.go(wait=True)
+        
         # Calling `stop()` ensures that there is no residual movement
         self.move_group.stop()
+        
         # It is always good to clear your targets after planning with poses.
         # Note: there is no equivalent function for clear_joint_value_targets()
         self.move_group.clear_pose_targets()
 
-        # we use the class variable rather than the copied state variable
+        # We use the class variable rather than the copied state variable
         current_pose = self.move_group.get_current_pose().pose
+        
+        # Return if the position we went to is within tolerances of the position we wanted
         return all_close(pose_goal, current_pose, 0.01)
 
 
 def main(file_path):
-    try:
-        robot_interface = MoveGroupPythonInterface()
+    """
+    Moves the end effector of the panda to the poses specified in the file given
 
+    Args:
+        file_path (string): the file path to the file containing poses
+    """
+    
+    try:
+        
+        # Initializes the move group interface
+        robot_interface = MoveGroupPythonInterface()
+        
+        # Parses the poses from the input file path
         pose_goals = parse_csv(file_path)
 
+        # Goes to every pose in the list of poses
         for pose_goal in pose_goals:
             print(pose_goal)
             robot_interface.go_to_pose(pose_goal[0], pose_goal[1], pose_goal[2])
 
+    # Ends the program if we get one of these errors
     except rospy.ROSInterruptException:
         return
     except KeyboardInterrupt:
@@ -171,7 +185,12 @@ def main(file_path):
 
 
 if __name__ == '__main__':
+    
+    # Default file we are parsing poses from
     file_path = "/home/joseph/ws_moveit/src/jgioia_panda_projects/csv/ex1.csv"
+    
+    # Overrides default if file is specified in arguments
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
+            
     main(file_path)
