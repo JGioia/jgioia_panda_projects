ff1c5c5001adac0ffa95fa748460ae32ea731700
Joseph <jeg4@rice.edu>
2021-03-03 21:11:09 -0600

    depth sensor work

---
+++ scripts/depth_sensor_interface.py
@@ -0,0 +1,34 @@
+"""Module that contains the DepthSensorInterface class"""
+
+import rospy
+import sensor_msgs
+
+
+class DepthSensorInterface():
+  """Provides useful methods for getting information from a depth sensor"""
+
+  def __init__(self, topic_name):
+    """Initializes the interface to get depth info from the specified topic.
+
+    Args:
+      topic_name (str): The name of the depth image topic. The topic must 
+        return a sensor_msgs.msg.Image.
+    """
+    rospy.init_node('DepthSensorInterface')
+    rospy.Subscriber(topic_name, sensor_msgs.msg.Image, self.update_image)
+
+  def update_image(self, depth_image):
+    """Updates the internal representation of the depth image.
+
+    Args:
+        depth_image (sensor_msgs.msg.Image): The new depth image
+    """
+    self.depth_image = depth_image
+
+  def detect_objects(self):
+    """Detects objects in the last depth image received
+    
+    Returns:
+      A list of poses of the objects detected.
+    """
+    return []
--- scripts/testing.py
+++ scripts/testing.py
@@ -2,14 +2,20 @@
 """An executable module where I test features. Currently working on pick and place."""
 
 import move_group_interface
+import depth_sensor_interface
 import rospy
+import time
 
 
 def main():
   """Runs testing code."""
   try:
-    robot_interface = move_group_interface.MoveGroupInterface()
-    robot_interface.test()
+    # robot_interface = move_group_interface.MoveGroupInterface()
+    # robot_interface.test()
+    depth_interface = depth_sensor_interface.DepthSensorInterface(
+        "camera/depth/image_raw")
+    time.sleep(5)
+    print(depth_interface.depth_image)
   except rospy.ROSInterruptException:
     return
   except KeyboardInterrupt:
